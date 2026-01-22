803149c2009da038a2aef39fd65aa23bb1653a59
codebydurvesh <durvesh.gaikwad08@gmail.com>
2026-01-22 17:29:14 +0530

    Add Tailwind CSS integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
-import "./App.css";
+import Home from "./pages/Home.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <Home />
+    </>
+  );
 }
 
 export default App;
